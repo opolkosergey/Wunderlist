149c184de0160830da86b57e27059d3e37ec5888
Vadim Mashuk <MashukW@gmail.com>
2016-02-29 22:55:22 +0300

    Add Log

---
+++ Wunderlist/EPAM.Wunderlist.Services/LoggerService/FileLoggerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NLog;
+
+namespace EPAM.Wunderlist.Services.LoggerService
+{
+    public class FileLoggerService : ILoggerService
+    {
+        private readonly Logger _logger;
+
+        public FileLoggerService()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public void Debug(string message) => _logger.Debug($"{message} at {DateTime.Now}");
+
+        public void Trace(string message) => _logger.Trace($"{message} at {DateTime.Now}");
+
+        public void Info(string message) => _logger.Info($"{message} at {DateTime.Now}");
+
+        public void Warn(string message) => _logger.Warn($"{message} at {DateTime.Now}");
+
+        public void Fatal(string message) => _logger.Fatal($"{message} at {DateTime.Now}");
+
+        public void Error(string message) => _logger.Error($"{message} at {DateTime.Now}");
+    }
+}
---
+++ Wunderlist/EPAM.Wunderlist.Services/LoggerService/ILoggerService.cs
@@ -0,0 +1,12 @@
+﻿namespace EPAM.Wunderlist.Services.LoggerService
+{
+    public interface ILoggerService
+    {
+        void Debug(string message);
+        void Trace(string message);
+        void Info(string message);
+        void Warn(string message);
+        void Fatal(string message);
+        void Error(string message);
+    }
+}
