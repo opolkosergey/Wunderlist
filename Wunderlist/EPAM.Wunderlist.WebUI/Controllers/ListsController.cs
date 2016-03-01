caa7be583b9c349cacb136ee3cecc4b36401034c
OpolkoSergey <opolkosergey@gmail.com>
2016-03-01 11:21:24 +0300

    add object service models

--- Wunderlist/EPAM.Wunderlist.WebUI/Controllers/ListsController.cs
+++ Wunderlist/EPAM.Wunderlist.WebUI/Controllers/ListsController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Web.Mvc;
+using EPAM.Wunderlist.Services.TodoListsService;
 
 namespace EPAM.Wunderlist.WebUI.Controllers
 {
     public class ListsController : Controller
     {
+        private ITodoListsService _listsService;
         public ActionResult Inbox()
         {
             return View();
